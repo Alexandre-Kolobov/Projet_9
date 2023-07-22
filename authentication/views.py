927658ab1b307a608ca3524ec4ad007c801791a3
Alexandre Kolobov <kolobovalexandre@gmail.com>
2023-07-22 18:46:56 +0200

    Ajout login, logout, signin

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.views.generic import View
+from authentication.forms import CreateUserForm
 
-# Create your views here.
+from . import forms
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def create_user(request):
+    form = CreateUserForm()
+    return render(request,
+        'authentication/user_create.html',
+        {'form': form})
+
+class CreateUserView(View):
+    form_class = forms.CreateUserForm
+    template_name = 'authentication/user_create.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, context={'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name, context={'form': form})
--- web_app/urls.py
+++ web_app/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import authentication.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', authentication.views.LoginPageView.as_view(), name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', blog.views.home, name='home'),
+    path('create_user/', authentication.views.CreateUserView.as_view(), name='create_user'),
 ]
